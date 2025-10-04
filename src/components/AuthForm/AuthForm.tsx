7cb61818826b2898be101157d7e670b363a71293
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-04 13:14:07 +0300

    feat: add basic form validation

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,13 +1,46 @@
 import { FormLabel } from "../FormLabel/FormLabel";
 import style from "./style.module.css";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { User } from "../../models/formSchema";
+
+export interface Inputs {
+  login: string;
+  password: string;
+}
 
 export function AuthForm() {
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(User),
+  });
+
+  function onSubmit(data: Inputs) {
+    console.log(data);
+  }
+
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={style.form_title}>Войти в кабинет:</h2>
 
-      <FormLabel text="логин" type="text" />
-      <FormLabel text="пароль" type="password" />
+      <FormLabel
+        text="логин"
+        name="login"
+        type="text"
+        register={register}
+        errors={errors}
+      />
+      <FormLabel
+        text="пароль"
+        name="password"
+        type="password"
+        register={register}
+        errors={errors}
+      />
       <div className={style["form_button-wrapper"]}>
         <button className={`${style.form_button} ${style.form_button__cancel}`}>
           Отмена
--- src/components/FormLabel/FormLabel.tsx
+++ src/components/FormLabel/FormLabel.tsx
@@ -1,15 +1,25 @@
+import type { FieldErrors, UseFormRegister } from "react-hook-form";
 import style from "./style.module.css";
+import type { Inputs } from "../AuthForm/AuthForm";
 
 interface Props {
   text: string;
   type: string;
+  name: "login" | "password";
+  register: UseFormRegister<Inputs>;
+  errors: FieldErrors<Inputs>;
 }
 
-export function FormLabel({ text, type }: Props) {
+export function FormLabel({ text, type, name, register, errors }: Props) {
+  const inputStyle = errors[name]
+    ? `${style.label_input} ${style.label_input__error}`
+    : `${style.label_input}`;
+
   return (
     <label className={style.label}>
       <div>Введите {text}:</div>
-      <input className={style.label_input} type={type} />
+      <input {...register(name)} className={inputStyle} type={type} />
+      {errors[name] && <p className={style.error}>{errors[name].message}</p>}
     </label>
   );
 }
